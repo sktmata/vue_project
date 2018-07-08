982a8022ba1d9ddd0568d9036549f86ed3c996fa
sun <853582668@qq.com>
2018-07-08 10:17:57 +0800

    elementUi

--- admin-vue/src/router/index.js
+++ admin-vue/src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/views/Login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'home',
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: {name: 'login'}
     }
   ]
 })
